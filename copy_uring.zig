a78a4186a29da1eb4b0af740115230056db45b4f
rmcdonald <richard_mcdonald@alumni.brown.edu>
2023-12-01 09:59:37 -0500

    working copy!
    There's a bug, sometimes it loops forever, oh well.

--- copy_uring.zig
+++ copy_uring.zig
@@ -3,9 +3,9 @@
 /// this program with mostly my own knowledge, having gone through the last 3
 /// examples of sync cat, raw uring cat, and zig uring cat. The problem and
 /// solution concept come from "io_uring by example" article series. The idea
-/// is this, given a IO_URING_DEPTH, keep the io_uring sqe queue as full
-/// as possible until the entire file is copied. So, we'll be adding as many
-/// reads to the sqe as we can, then reading any available completed reads, and
+/// is this, given a RING_DEPTH, keep the io_uring sqe queue as full as
+/// possible until the entire file is copied. So, we'll be adding as many reads
+/// to the sqe as we can, then reading any available completed reads, and
 /// submitting as many writes as possible, and on an on until we're done. This
 /// presupposes that we read and write in chunks (16kb in the article). This is
 /// the general concept from the article. There may be more interesting things
@@ -22,10 +22,13 @@ const BUFF_SIZE = 4 * 1024;
 const CHUNK_SIZE = 16 * 1024;
 
 const UserData = struct {
-    off: u64, 
+    off: u64,
     bytes: u64,
     iovecs: []os.iovec,
+    // It was annoying trying to convert []os.iovec to this type for the writev
+    // calls, I'm sure there's a way but I'm being lazy and just doing this
     iovecs_const: []const os.iovec_const,
+    is_read: bool,
 };
 
 fn create_user_data(allocator: *Allocator, off: u64, bytes: u64) !*UserData {
@@ -57,13 +60,13 @@ fn create_user_data(allocator: *Allocator, off: u64, bytes: u64) !*UserData {
         };
         i += 1;
         bytes_remaining -= bytes_prepped;
-        
     }
-    user_data.* = UserData {
+    user_data.* = UserData{
         .off = off,
         .bytes = bytes,
         .iovecs = iovecs,
         .iovecs_const = iovecs_const,
+        .is_read = true,
     };
 
     return user_data;
@@ -80,45 +83,69 @@ fn copy(allocator: *Allocator, ring: *linux.IO_Uring, src: []const u8, dst: []co
     if (@mod(total_bytes, CHUNK_SIZE) != 0) {
         total_reads += 1;
     }
+    var read_bytes_submitted: u64 = 0;
     var bytes_read: u64 = 0;
+    var write_bytes_submitted: u64 = 0;
     var bytes_written: u64 = 0;
-    while (bytes_read < total_bytes or bytes_written < total_bytes) {
-        if (bytes_read < total_bytes) {
+    var total_cqes: u64 = 0;
+    var total_sqes: u64 = 0;
+    while (read_bytes_submitted < total_bytes or write_bytes_submitted < total_bytes) {
+        if (read_bytes_submitted < total_bytes) {
             const sqes_ready = ring.sq_ready();
             var to_read = sqe_len - sqes_ready;
-            while (to_read > 0) {
-                var bytes_remaining = @as(u64, @intCast(total_bytes)) - bytes_read;
+            while (to_read > 0 and read_bytes_submitted < total_bytes) {
+                var bytes_remaining = @as(u64, @intCast(total_bytes)) - read_bytes_submitted;
                 if (bytes_remaining > CHUNK_SIZE) {
                     bytes_remaining = CHUNK_SIZE;
                 } else {
                     to_read = 1;
                 }
 
-                const user_data = try create_user_data(allocator, bytes_read, bytes_remaining);
+                const user_data = try create_user_data(allocator, read_bytes_submitted, bytes_remaining);
                 const read_buffer: linux.IO_Uring.ReadBuffer = .{
                     .iovecs = user_data.iovecs,
                 };
                 _ = try ring.read(@intFromPtr(user_data), src_fd, read_buffer, user_data.off);
+                total_sqes += 1;
                 to_read -= 1;
-                bytes_read += bytes_remaining;
+                read_bytes_submitted += bytes_remaining;
             }
             _ = try ring.submit();
         }
         var cqes_ready = ring.cq_ready();
-        // std.debug.print("cqes_ready: {any}\n", .{cqes_ready});
         const sqes_ready = ring.sq_ready();
         var to_write = sqe_len - sqes_ready;
-        // std.debug.print("sqes_ready: {any}\n", .{sqes_ready});
-        // std.debug.print("writes_submitted: {any}\n", .{writes_submitted});
-        while (cqes_ready > 0 and to_write > 0) {
+        while (cqes_ready != 0 and to_write != 0) {
             const cqe = try ring.copy_cqe();
+            cqes_ready -= 1;
+            total_cqes += 1;
             const user_data_ptr = cqe.user_data;
             const user_data: *UserData = @ptrFromInt(user_data_ptr);
-            _ = try ring.writev(@intFromPtr(user_data), dst_fd, user_data.iovecs_const, user_data.off);
-            to_write -= 1;
-            cqes_ready -= 1;
-            bytes_written += user_data.bytes;
+            if (user_data.bytes != cqe.res) {
+                // Not handling this but is nice to know if it happens
+                std.debug.print("partial io!!\nread?: {any}\nsubmitted: {any}\ncompleted: {any}\n", .{ user_data.is_read, user_data.bytes, cqe.res });
+            }
+            if (user_data.is_read) {
+                const is_read = &user_data.*.is_read;
+                is_read.* = false;
+                _ = try ring.writev(@intFromPtr(user_data), dst_fd, user_data.iovecs_const, user_data.off);
+                to_write -= 1;
+                write_bytes_submitted += user_data.bytes;
+                bytes_read += @intCast(cqe.res);
+            } else {
+                bytes_written += @intCast(cqe.res);
+            }
         }
+        if (ring.sq_ready() > 0) {
+            _ = try ring.submit();
+        }
+        // std.debug.print("total_reads: {any}\n", .{total_reads});
+        // std.debug.print("read_bytes_submitted: {any}\n", .{read_bytes_submitted});
+        // std.debug.print("bytes_read: {any}\n", .{bytes_read});
+        // std.debug.print("write_bytes_submitted: {any}\n", .{write_bytes_submitted});
+        // std.debug.print("bytes_written: {any}\n", .{bytes_written});
+        // std.debug.print("total_sqes: {any}\n", .{total_sqes});
+        // std.debug.print("total_cqes: {any}\n", .{total_cqes});
     }
 }
 
